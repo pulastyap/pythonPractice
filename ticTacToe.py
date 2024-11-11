dffc90c30e1070f5b90e408a7b21309dab40f28a
pulastyap <pulastyapandey4444@gmail.com>
2024-11-10 18:24:56 -0600

    Create ticTacToe.py

---
+++ ticTacToe.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+def getFirstTurn():
+    choice = input('Do you want to go first? Enter Y/y if you want to go first: ')
+    if choice in ['Y', 'y']:
+        print('User will start first')
+        return 'U'
+    else:
+        print('Computer will start first')
+        return 'C'
+    
+def startTicTacToe():
+    ticTacToe = []    
+
+def ticTacToe():
+    firstTurn = getFirstTurn()
+
+
+
+
+#Calling main function
+ticTacToe()
