a282ff75d2c424f510baf331d86247db281fed79
pulastyap <pulastyapandey4444@gmail.com>
2025-01-02 13:31:17 -0600

    Latest

---
+++ Dictionary/createDict.py
@@ -0,0 +1,33 @@
+
+name= "John"
+age= 21
+grade= "A"
+
+student = dict(name, age, grade)
+print(student)
+print(type(student))
+
+
+
+
+'''
+student = dict(name= "John", age= 21, grade= "A")
+print(student)
+print(type(student))
+
+
+student = {"name": "John", "age": 21, "grade": "A"}
+stud_contact_details = {"name": "John", 
+                        "contact no": {"mobile no": 2242242242,
+                                       "home phone no": 4424424424},
+                        "address": {"street address": "N ABC road",
+                                    "unit no" : 100},
+                                    "city" : "Chicago",
+                                    "state": "IL",
+                                    "zip code": 60010}
+
+print(student)
+print(type(student))
+print(stud_contact_details)
+print(type(stud_contact_details))
+'''
